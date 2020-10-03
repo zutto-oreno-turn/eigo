3f1ee53ee2969bd1a7dccff3254e624e4cce646f
Koichi Yamamoto <kotaro36@users.noreply.github.com>
2020-10-04 00:24:32 +0900

    Add category.json

--- Assets/Eigo/Scripts/CategoryManager.cs
+++ Assets/Eigo/Scripts/CategoryManager.cs
@@ -9,9 +9,25 @@ public class CategoryManager : MonoBehaviour
     public GameObject CategoryPanel;
     public GameObject CategoryButtonPrefab;
 
-    void Start() {
+    void Start()
+    {
         Debug.Log("CategoryManager.cs#Start");
-        GameObject categoryButton = Instantiate(CategoryButtonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        categoryButton.transform.SetParent(CategoryPanel.transform, false);
+
+        // https://zutto-oreno-turn.github.io/cdn/eigo/question/category.json
+        
+        // GameObject categoryButton = Instantiate(CategoryButtonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        // categoryButton.transform.SetParent(CategoryPanel.transform, false);
+        // categoryButton.GetComponent<Button>().onClick.AddListener(CallOnClick);
     }
+
+    // void CallOnClick()
+    // {
+    //     Debug.Log("CategoryManager.cs#CallOnClick");
+    //     MyOnClick();
+    // }
+
+    // void MyOnClick()
+    // {
+    //     Debug.Log("CategoryManager.cs#MyOnClick");
+    // }
 }
